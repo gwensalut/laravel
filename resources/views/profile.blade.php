a07d2c89d979c6d627d6583ba2d1ddcf6c398bde
gwensalut <gwensalut15@gmail.com>
2023-05-25 11:16:14 +0800

    laravel making
    pogi si teddy

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="">LANDING PAGE</a>
+            <a href="">HOME</a>
+            <a href="">PROFILE</a>
+            <a href="">EVENTS</a>
+            <a href="">NEWS</a>
+            <a href="">CONTACTS</a>
+        </nav>
+    </header>
+    <h1>WELCOME TO LANDING PAGE</h1>
+</body>
+</html>
